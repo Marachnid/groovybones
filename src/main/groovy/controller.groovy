b69e88aef01bee48e544de1c7100180a78e0d8b4
Marachnid <matthew_c_13@outlook.com>
2026-02-28 19:42:54 -0600

    Backing up rough code for difficulty logic

--- src/main/groovy/controller.groovy
+++ src/main/groovy/controller.groovy
@@ -15,15 +15,62 @@ GameBoard test2 = new GameBoard(boardName: 'player2')
 boolean runGame = true
 
 
-while (runGame) {
-    dice = r.nextInt(6) + 1
-    test1.runBoard(dice)
+test1.board[0] << 1
+test1.board[0] << 1
+test1.board[0] << 1
+test1.board[1] << 2
+test1.board[2] << 3
+test1.board[2] << 1
 
-    if (test1.detectFullBoard()) {
-        break
+
+
+println test1.board
+
+
+def test = test1.mapBoardValues()
+
+println test['index']
+println test['number']
+println test['repetitions']
+
+println test[0]
+
+//if (test.any {it.number == 1}) {
+//    println '1 DETECTED'
+//}
+
+def meh = 4
+
+test.any {
+
+    if (it.number == meh) {
+
+        if (test2.addNumber(it.index, it.number)) {
+            test1.deleteNumber(it.index, it.number)
+        } else {
+            test2.runBoard(meh)
+        }
     } else {
-        dice = r.nextInt(6) + 1
-        test2.runBoard(dice)
+        test2.runBoard(meh)
+    }
+}
+
+
+def mehTest = test2.mapBoardValues()
+meh = 4
+
+
+mehTest.any {
+
+    if (it.number == meh) {
+
+        if (test1.addNumber(it.index, it.number)) {
+            test2.deleteNumber(it.index, it.number)
+        } else {
+            test1.runBoard(meh)
+        }
+    } else {
+        test1.runBoard(meh)
     }
 }
 
@@ -31,9 +78,25 @@ while (runGame) {
 println test1.board
 println test2.board
 
-test1.calculateScore()
-test2.calculateScore()
-
-
-println test1.score
-println test2.score
+//while (runGame) {
+//    dice = r.nextInt(6) + 1
+//    test1.runBoard(dice)
+//
+//    if (test1.detectFullBoard()) {
+//        break
+//    } else {
+//        dice = r.nextInt(6) + 1
+//        test2.runBoard(dice)
+//    }
+//}
+//
+//
+//println test1.board
+//println test2.board
+//
+//test1.calculateScore()
+//test2.calculateScore()
+//
+//
+//println test1.score
+//println test2.score
