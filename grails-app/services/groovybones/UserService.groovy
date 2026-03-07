0f6bb832ca95893f7091aa0aed3c29db0423953e
Marachnid <matthew_c_13@outlook.com>
2026-03-06 20:00:05 -0600

    Refactored UserService for updated user table

--- grails-app/services/groovybones/UserService.groovy
+++ grails-app/services/groovybones/UserService.groovy
@@ -6,40 +6,37 @@ import user.User
 @Transactional
 class UserService {
 
-
     /**
-     * creates a new DB entity
-     * @param firstName user's first name
-     * @param userName user's username
-     * @return new user added to DB
+     * initializes and creates a new User entity
+     * @param firstName first name of user
+     * @param userName user's userName
+     * @return new user with pre-initialized wins/losses/totalScore = 0
      */
-    def createUser(def firstName, def userName) {
-        def user = new User(firstName: firstName, userName: userName)
+    User createUser(String firstName, String userName) {
+        User user = new User(firstName: firstName, userName: userName, wins: 0, losses: 0, totalScore: 0)
         user.save(failOnError: true)
         return user
     }
+
+
+    /**
+     * updates a User entity
+     * @param user entity to be updated
+     * @return updated User entity
+     */
+    User updateUser(User user) {
+        user.save(failOnError: true)
+        return user
+    }
+
 
     /**
      * deletes an existing DB entity
      * @param id id to be deleted
      * @return existing user to be deleted from DB
      */
-    def deleteUser(int id) {
-        def user = User.get(id)
+    def deleteUser(def id) {
+        def user = User.get(id as int)
         user.delete(failOnError: true)
-    }
-
-
-    /**
-     * updates an existing entity's username
-     * @param id user id to be updated
-     * @param newName new user name
-     * @return updated userName to be saved to DB
-     */
-    def updateUserName(int id, def newName) {
-        def user = User.get(id)
-        user.userName = newName
-        user.save(failOnError: true)
-        return user
     }
 }
