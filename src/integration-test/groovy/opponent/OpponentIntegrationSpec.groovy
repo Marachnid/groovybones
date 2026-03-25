de7e3ff62fc47ab89c47c2f6bd8daa6bb3d33e38
Marachnid <matthew_c_13@outlook.com>
2026-03-24 23:22:56 -0500

    implemented integration tests

--- src/integration-test/groovy/opponent/OpponentIntegrationSpec.groovy
+++ src/integration-test/groovy/opponent/OpponentIntegrationSpec.groovy
@@ -3,20 +3,36 @@ package opponent
 import grails.testing.mixin.integration.Integration
 import grails.gorm.transactions.*
 import groovybones.OpponentService
+import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Stepwise
+import util.SQLRunner
+import javax.sql.DataSource
 
 @Integration
 @Rollback
+@Stepwise
 class OpponentIntegrationSpec extends Specification {
+    @Shared
+    DataSource dataSource
+
     Opponent opponent
     OpponentService opService
 
 
+    /** basic setup method to instantiate objects and refresh test schema*/
     def setup() {
+        new SQLRunner(dataSource).recreateOpponentTable()
+
+        opService = new OpponentService()
         opponent = new Opponent(username: 'tester', difficulty: 1, wins: 0, losses: 0, totalScore: 0)
                 .save(failOnError: true)
     }
 
+    /**
+     * tests that opponent difficulty update is ignored
+     * @return pass/fail
+     */
     def "opponentUpdate() ignores updates to difficulty"() {
         when: 'opponent difficulty is changed'
         opponent.difficulty = 2
@@ -24,29 +40,62 @@ class OpponentIntegrationSpec extends Specification {
         and: 'the opponent is updated'
         opponent = opService.updateOpponent(opponent)
 
-
-
         then: 'opponent difficulty remains unchanged'
         opponent.difficulty == 1
     }
-}
 
+    /**
+     * tests an update to username fails to find existing user
+     * @return pass/fail
+     */
+    def "updateOpponent() fails to find username to update"() {
+        when: 'opponent name is changed'
+        opponent.username = 'tester2'
 
+        and: 'opponent tries to update'
+        opponent = opService.updateOpponent(opponent)
 
-
-/*
-
-
-
-
-    def 'updateOpponent() will not find opponent by new username'() {
-        when: 'opponent username is changed'
-        opponent.username = 'tester1'
-
-//        and: 'updateOpponent() attempts to find existing opponent'
-//        opponent.updateOpponent()
-
-        then: ''
-        opponent.updateOpponent() == null
+        then: 'updateOpponent() throws null exception'
+        thrown(NullPointerException)
     }
- */
+
+    /**
+     * tests successfully updating opponent wins, losses, totalScore
+     * @return pass/fail
+     */
+    def "updateOpponent() successfully updates opponent"() {
+        when: 'opponent wins, losses, totalScore is updated'
+        opponent.wins = 1
+        opponent.losses = 1
+        opponent.totalScore = 1
+
+        and: 'opponent is updated and reassigned to a temp object'
+        Opponent newOp = opService.updateOpponent(opponent)
+
+        then: 'temp opponent should match wins, losses, totalScore'
+        newOp.wins == opponent.wins
+        newOp.losses == opponent.losses
+        newOp.totalScore == opponent.totalScore
+    }
+
+    /**
+     * tests overall successful update that results in difficulty ignored
+     * @return pass/fail
+     */
+    def "updateOpponent() updates wins, losses, totalScore and ignores difficulty"() {
+        when: 'opponent wins, losses, totalScore, and difficulty is updated'
+        opponent.wins = 1
+        opponent.losses = 1
+        opponent.totalScore = 1
+        opponent.difficulty = 2
+
+        and: 'opponent is updated and reassigned to a temp object'
+        Opponent newOp = opService.updateOpponent(opponent)
+
+        then: 'temp opponent should match wins, losses, totalScore, but not difficulty'
+        newOp.wins == opponent.wins
+        newOp.losses == opponent.losses
+        newOp.totalScore == opponent.totalScore
+        newOp.difficulty != opponent.difficulty
+    }
+}
