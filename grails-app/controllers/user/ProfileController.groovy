e4f869c3817e7d2760607b7b91c4f33bbd490b3a
Marachnid <matthew_c_13@outlook.com>
2026-04-08 10:03:43 -0500

    Built out ProfileController

--- grails-app/controllers/user/ProfileController.groovy
+++ grails-app/controllers/user/ProfileController.groovy
@@ -1,19 +1,59 @@
 package user
 
+import groovybones.user.UserService
+
 
 /**
- * Responsible for rendering Profile page
+ * Responsible for profile page and management
+ * Shows accumulated stats
+ * Allows bulk deletes of saved games
+ * Can reset stats back to 0
+ * Can reset account
  */
 class ProfileController {
 
     /**
-     * default method to return profile page
-     * @return render profile.gsp
+     * handles rendering profile and loading/updating basic user stats into/in session
+     * @return render profile
      */
     def profile() {
-        println 'ProfileController Profile()'
+        log.info('ProfileController profile()')
 
-        if (session['user']) render(view: 'profile')
-        else redirect(controller: 'home', action: 'index')
+        Map userStats = new UserService().getUserStats(session['userId'] as Long)
+
+        userStats.totalGames = userStats?.wins + userStats?.losses
+        if (userStats.totalGames == 0) userStats.winPercentage = 0
+        else userStats.winPercentage = (int) ((userStats.wins / (userStats.wins + userStats.losses)) * 100)
+
+        session['userStats'] = userStats
+
+        render(view: 'profile')
+    }
+
+
+    /**
+     * resets user's stats back to 0
+     * @return redirect back to profile to update session
+     */
+    def resetStats() {
+        log.info('ProfileController resetStats()')
+
+        Map userStats = [wins: 0, losses: 0, totalScore: 0]
+        new UserService().updateUser(session['userId'] as Long, userStats)
+        redirect(controller: 'profile', action: 'profile')
+    }
+
+
+    /**
+     * resets a user's profile by deleting and invalidating in session
+     * fresh profile is created upon signing in
+     * @return redirect back to home
+     */
+    def resetProfile() {
+        log.info('ProfileController resetProfile()')
+
+        new UserService().deleteUser(session['userId'] as Long)
+        session.invalidate()
+        redirect(controller: 'home', action: 'index')
     }
 }
